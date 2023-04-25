c4f259fbb9806d8489de9451a5f21452ba1887fd
Ashkan <92475017+NoobstrGTR@users.noreply.github.com>
2023-04-25 12:36:22 +0430

    order by id api is added

--- backend/core/base/urls/orders_urls.py
+++ backend/core/base/urls/orders_urls.py
@@ -4,5 +4,7 @@ from core.base.views import order_views as views
 
 
 urlpatterns = [
-
+    path('add/', views.addOrderItems, name='orders-add'),   
+    path('<str:pk>/', views.getOrderById, name='user-order'),   
+    
 ]
---
+++ backend/core/base/views/order_views.py
@@ -0,0 +1,73 @@
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+
+from django.contrib.auth.hashers import make_password
+
+from core.base.models import Product, Order, OrderItem, ShippingAddress
+from core.base.serializers import ProductSerializer, OrderSerializer
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+    orderItems = data['orderItems']
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No order items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # (1) Create a order
+        order = Order.objects.get(
+            user=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+        # (2) Create Shipping address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country'],
+
+        )
+        # (3) Create order items and set order to orderItem relationship
+        for i in orderItems:
+            product = Product.objects.get(_id=i['product'])
+
+            item = OrderItem.objects.create(
+                product=product,
+                order=order,
+                name=product.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=product.image.url
+            )
+
+        # (4) Update stock
+        product.countInStock -= item.qty
+        product.save()
+
+    serializer = OrderSerializer(order, many=True)
+    return Response(serializer.data)
+
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def getOrderById(request, pk):
+    user = request.user
+    
+    try:
+        order = Order.objects.get(_id=pk)
+        if user.is_staff or order.user == user:
+            serializer = OrderSerializer(order, many=False)
+            return Response(serializer.data)
+        else:
+            Response({'detail': 'You are not authorized to view this order'}, status=status.HTTP_401_UNAUTHORIZED)
+    except:
+        return Response({'detail': 'Order not found'}, status=status.HTTP_404_NOT_FOUND)
