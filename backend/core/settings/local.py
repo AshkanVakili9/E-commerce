0d6ab575d2ea5e92767cc636ee3812260fdfe546
Ashkan <92475017+NoobstrGTR@users.noreply.github.com>
2023-04-25 12:41:21 +0430

    Update local.py

--- backend/core/settings/local.py
+++ backend/core/settings/local.py
@@ -4,9 +4,21 @@ from .base_settings import *
 
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Ecommerce',
+        'USER': 'postgres',
+        'PASSWORD': 'araa23o385',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
